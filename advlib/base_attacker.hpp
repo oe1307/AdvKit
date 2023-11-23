db13d53b93744d51d2bc8ee57db60f45ab7febe4
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2023-11-23 09:56:12 +0900

    update

--- advlib/base_attacker.hpp
+++ advlib/base_attacker.hpp
@@ -8,28 +8,24 @@ using dict = std::map<std::string, std::string>;
 
 class Config {
    public:
-    int device;
-    int thread;
-    int seed;
     std::string attacker;
-    std::string dataset;
-    std::string model;
-    int batch_size;
-    int n_examples;
-    int iteration;
     std::string norm;
     float epsilon;
+    std::string dataset;
+    std::string model;
+    int n_examples;
+    int batch_size;
+    int device;
 
     explicit Config(dict args) {
-        this->device = stoi(args["device"]);
-        this->thread = stoi(args["thread"]);
-        this->seed = stoi(args["seed"]);
         this->attacker = args["attacker"];
-        this->dataset = args["dataset"];
-        this->n_examples = stoi(args["n_examples"]);
-        this->iteration = stoi(args["iteration"]);
         this->norm = args["norm"];
         this->epsilon = stof(args["epsilon"]);
+        this->dataset = args["dataset"];
+        this->model = args["model"];
+        this->n_examples = stoi(args["n_examples"]);
+        this->batch_size = stoi(args["batch_size"]);
+        this->device = stoi(args["device"]);
     }
 
     // TODO: 出力演算子
@@ -37,7 +33,6 @@ class Config {
 
 class Attacker {
    public:
-    explicit Attacker(Config config) : config(config) {}
     void attack();
 
    private:

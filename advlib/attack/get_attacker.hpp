8c3fb309368c124cdfd14f36fd3afae6d0ec478f
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2023-11-24 10:45:28 +0900

    update

--- advlib/attack/get_attacker.hpp
+++ advlib/attack/get_attacker.hpp
@@ -10,12 +10,13 @@
 namespace advlib {
 
 inline Attacker* get_attacker() {
-    Parameter config = config_parser().get();
-    if (config.attacker == "Fgsm") {
-        return new Fgsm();
-    } else {
-        throw std::invalid_argument("Invalid attacker: " + config.attacker);
-    }
+    // Parameter config = config_parser().get();
+    // if (config.attacker == "Fgsm") {
+    //     return new Fgsm();
+    // } else {
+    //     throw std::invalid_argument("Invalid attacker: " + config.attacker);
+    // }
+    return new Fgsm();
 }
 
 }  // namespace advlib
--- tests/advlib/attack/whitebox/test_fgsm.cpp
+++ tests/advlib/attack/whitebox/test_fgsm.cpp
@@ -1,6 +1,6 @@
 #include "../../../../advlib/advlib.hpp"
 
 int main() {
-    auto config = advlib::config_parser();
+    // auto config = advlib::config_parser();
     return 0;
 }

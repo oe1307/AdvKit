47017575ad3f4f325ec3ec119ad5baea10bf9c1e
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2024-02-22 14:54:32 +0900

    update

--- bin/download_robustbench.py
+++ bin/download_robustbench.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python3
+
+import os
 import sys
 
 from robustbench import load_model
+from robustbench.model_zoo.enums import BenchmarkDataset, ThreatModel
 
-model = sys.argv[1]
+model = "Carmon2019Unlabeled"
+dataset = BenchmarkDataset.cifar_10
+norm = ThreatModel.Linf
+savedir = os.path.dirname(sys.argv[0]) + "/../storage"
 
-model = load_model(model, root, dataset, norm=norm)
+model = load_model(model, savedir, dataset, norm=norm)

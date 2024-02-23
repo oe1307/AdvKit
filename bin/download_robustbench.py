fb274eb802dc36775a37e644a727bcd69447f6a6
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2024-02-23 13:06:55 +0900

    update

--- bin/download_robustbench.py
+++ bin/download_robustbench.py
@@ -3,12 +3,24 @@
 import os
 import sys
 
+import torch
 from robustbench import load_model
 from robustbench.model_zoo.enums import BenchmarkDataset, ThreatModel
 
-model = "Carmon2019Unlabeled"
+model_name = "Carmon2019Unlabeled"
 dataset = BenchmarkDataset.cifar_10
 norm = ThreatModel.Linf
 savedir = os.path.dirname(sys.argv[0]) + "/../storage"
 
-model = load_model(model, savedir, dataset, norm=norm)
+if dataset in (BenchmarkDataset.cifar_10, BenchmarkDataset.cifar_100):
+    tmp_input = torch.randn(1, 3, 32, 32)
+else:
+    raise NotImplementedError(dataset)
+
+model = load_model(model_name, savedir, dataset, norm=norm).eval()
+
+try:
+    model_script = torch.jit.script(model)
+except Exception:
+    model_script = torch.jit.trace(model, tmp_input)
+model_script.save(f"{savedir}/{dataset.value}/{norm.value}/{model_name}_jit.pt")
--- src/model.cpp
+++ src/model.cpp
@@ -1,7 +1,10 @@
 #include "./model.hpp"
 
+#include <torch/torch.h>
+
 advlib::Model advlib::get_model(advlib::Config config) {
     //
-    Model model;
+    std::string model_path = "storage/cifar10/Linf/Carmon2019Unlabeled_jit.pt";
+    Model model = torch::jit::load(model_path);
     return model;
 }

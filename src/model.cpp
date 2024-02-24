cc6f492d9572520f0baa6f44cad3bf3b45c1c113
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2024-02-24 20:44:53 +0900

    update

--- src/model.cpp
+++ src/model.cpp
@@ -3,7 +3,7 @@
 #include <torch/torch.h>
 
 advlib::Model advlib::get_model(advlib::Config config) {
-    std::string model_path = "storage/cifar10/Linf/Carmon2019Unlabeled_jit.pt";
+    std::string model_path = "tests/example/resnet18.pt";
     Model model = torch::jit::load(model_path);
     model.to(config.device);
     return model;

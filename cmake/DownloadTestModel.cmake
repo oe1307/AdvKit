52672b436fb36774973d6f3d8af71d5b972b58b5
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2024-02-28 16:27:37 +0900

    update

--- cmake/DownloadTestModel.cmake
+++ cmake/DownloadTestModel.cmake
@@ -4,6 +4,14 @@ if(NOT Python3_FOUND)
   message(FATAL_ERROR "Python3 not found")
 endif()
 
-execute_process(COMMAND python3 -m pip install --upgrade pip)
-execute_process(COMMAND python3 -m pip install torchvision requests)
-execute_process(COMMAND python3 tests/example/download_model.py)
+execute_process(COMMAND "python3 -m pip install torchvision requests"
+                RESULT_VARIABLE return)
+if(NOT return EQUAL 0)
+  message(FATAL_ERROR "Failed to install requirements")
+endif()
+
+execute_process(COMMAND "python3 -m pip install -r tests/requirements.txt"
+                RESULT_VARIABLE return)
+if(NOT return EQUAL 0)
+  message(FATAL_ERROR "Failed to download test model")
+endif()
